c4cc8932c07f1f419d31838c46162f104312a062
Cal Henderson <cal@iamcal.com>
2011-12-29 14:41:19 -0800

    put tileset config in its own var so we can swap them out for outlands/deepholm/etc

--- map2.js
+++ map2.js
@@ -1,12 +1,27 @@
 var map_config = {
-	path		: '/tiles/pngs2/',
 	fileExt		: 'png',
 	tileSize	: 256,
 	defaultZoom	: 0,
-	maxZoom		: 4,
 	cacheMinutes	: 0,
 	debug		: false
 };
+
+var tiles_config = {
+	stem		: '/tiles/built/azeroth/',
+	center		: [0.9, 1.3],
+	bgcolor		: '#001D29',
+	layers		: {
+		5: [3,2],
+		4: [6,4],
+		3: [12,8],
+		2: [99,99], //?
+		1: [99,99], //?
+		0: [84,59]  //?
+	}
+};
+
+
+
 
 //
 // a very simple projection
@@ -44,37 +59,34 @@ function PixelsToLatLng(pxs){
 // the class for our map layer
 //
 
-//Zoom map
-//0 -> 4
-//1 -> 3
-//2 -> 2
-//3 -> 1
+var num_layers = 0;
+for (var i in tiles_config.layers) num_layers++;
 
 var WoWMapOptions = {
 
 	getTileUrl: function(tile, zoom) {
 
+		//console.log(tile.x, tile.y);
 
-		//if (x < 0 || y < 0) return null;
-		//if ((zoom == 3) && (tile.x > 15 || tile.y > 12)) return null;
-		//if ((zoom == 2) && (tile.x > 7 || tile.y > 6)) return null;
-		//if ((zoom == 1) && (tile.x > 3 || tile.y > 3)) return null;
-		//if ((zoom == 0) && (tile.x > 1 || tile.y > 1)) return null;
+		if (tile.x < 0 || tile.y < 0) return null;
 
-		var z = 1 + (4 - zoom);
+		var z = 1 + ((num_layers-2) - zoom);
+
+		if (tiles_config.layers[z][0] <= tile.x) return null;
+		if (tiles_config.layers[z][1] <= tile.y) return null;
 
 		var tx = ""+tile.x;
 		var ty = ""+tile.y;
 		while (tx.length < 2) tx = "0"+tx;
 		while (ty.length < 2) ty = "0"+ty;
 
-		var url = '/tiles/built/azeroth/tile_z'+z+'_'+tx+'_'+ty+'.png';
-		console.log(tile, url);
-		return url;
+		var url = tiles_config.stem+'tile_z'+z+'_'+tx+'_'+ty+'.png';
+		//console.log(tile, url);
+
 		return url;
 	},
 	tileSize: new google.maps.Size(map_config.tileSize, map_config.tileSize),
-	maxZoom:  map_config.maxZoom,
+	maxZoom:  num_layers-1,
 	minZoom:  0,
 	isPng:    false
 };
@@ -90,10 +102,9 @@ var map;
     
 function initialize() {
 	var mapOptions = {
-		backgroundColor: '#001D29',
+		backgroundColor: tiles_config.bgcolor,
 		zoom: map_config.defaultZoom,
-		center2: PixelsToLatLng(256, 25),
-		center: new google.maps.LatLng(0, 0),
+		center: new google.maps.LatLng(tiles_config.center[0], tiles_config.center[1]),
 		navigationControl: true,
 		navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL }, // still no way to get rid of the dude
 		scaleControl: false,
