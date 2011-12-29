a27a20e57a2383ade8fab6ac924f58fe6ae6f25d
Cal Henderson <cal@iamcal.com>
2011-12-29 12:43:41 -0800

    gathering all the needed tilesets to make azeroth (it is many!)

--- build.php
+++ build.php
@@ -4,12 +4,20 @@
 
 	clean_set('azeroth');
 
-	build_set('azeroth', 'Kalimdor/map', 23, 48, 9, 55, 8, 6);
-	build_set('azeroth', 'Expansion01/map', 50, 59, 32, 42, 0, 11, array('58_32'));
+	build_set('azeroth', 'Kalimdor/map', 23, 48, 9, 55, 8, 6+7); # kalimdor
+	build_set('azeroth', 'Expansion01/map', 50, 59, 32, 42, 0, 11+7, array('58_32')); # exodar
 
-	build_set('azeroth', 'Azeroth/map', 17, 45, 22, 61, 34, 12);
-	build_set('azeroth', 'Expansion01/map', 41, 51, 6, 20, 53, 0, array('46_20', '47_20', '40_20', '41_20', '42_20'));
+	build_set('azeroth', 'Azeroth/map', 17, 45, 22, 61, 34+21, 12+7); # eastern kingdoms
+	build_set('azeroth', 'Expansion01/map', 41, 51, 6, 20, 53+21, 0+7, array('46_20', '47_20', '40_20', '41_20', '42_20')); # silvermoon city & isle
 
+	build_set('azeroth', 'Northrend/map', 16, 45, 11, 33, 33, 0); # northrend
+
+	build_set('azeroth', 'LostIsles/map', 26, 32, 45, 51, 41, 53); # kezan
+	build_set('azeroth', 'LostIsles/map', 24, 31, 26, 32, 33, 46); # lost isles
+
+	build_set('azeroth', 'TolBarad/map', 27, 32, 31, 37, 57, 30); # tol barad
+
+	build_set('azeroth', 'MaelstromZone/map', 28, 32, 28, 32, 46, 27); # maelstrom
 
 	function build_set($set_name, $src_path, $min_x, $max_x, $min_y, $max_y, $offset_x, $offset_y, $skips=array()){
 
