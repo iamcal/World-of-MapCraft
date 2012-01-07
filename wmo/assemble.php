80cd1266d5dc0d52252ed5305d3142f5c19fe0c1
Cal Henderson <cal@iamcal.com>
2012-01-06 18:07:15 -0800

    importing the current state of the wmo assmebler - looking good!

---
+++ wmo/assemble.php
@@ -0,0 +1,129 @@
+<?
+	include('../build/config.php');
+
+	#assemble_set('inst_rfc', array(
+	#	array('LavaDungeon.png', 0, 0),
+	#));
+
+	#assemble_set('inst_wc', array(
+	#	array('wailingcaverns_instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_sm', array(
+	#	array('Monestary_Cathedral.png', 563, 12),
+	#	array('Monestary_Cemetery.png', 0, 622),
+	#	array('Monestary_War.png', 1200, 0),
+	#	array('Monestary_Library.png', 1200, 1233),
+	#), 'white');
+
+	#assemble_set('inst_bfd', array(
+	#	array('Blackfathom_instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_stocks', array(
+	#	array('StormwindJail.png', 0, 0),
+	#));
+
+	#assemble_set('inst_gnomer', array(
+	#	array('KZ_Gnomeragon_Instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_rfk', array(
+	#	array('RazorfenKraul_instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_rfd', array(
+	#	array('RazorfenDowns_instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_mara', array(
+	#	array('KL_Maraudon_instance01.png', 0, 0),
+	#));
+
+	#assemble_set('inst_ulda', array(
+	#	array('KZ_Uldaman_A.png', 727, 359),
+	#	array('KZ_Uldaman_B.png', 0, 0),
+	#));
+
+	#assemble_set('inst_dm', array(
+	#	array('KL_Diremaul_Instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_strat', array(
+	#	array('Stratholme.png', 0, 0),
+	#));
+
+	#assemble_set('raid_naxx', array(
+	#	array('Stratholme_raid.png', 0, 0),
+	#	array('FrostWyrm_Final01.png', 2200, 250),
+	#));
+
+	#assemble_set('raid_mc', array(
+	#	array('Blackrock_lower_guild.png', 0, 0),
+	#));
+
+	#assemble_set('inst_brd', array(
+	#	array('Blackrock_lower_instance.png', 0, 0),
+	#));
+
+	#assemble_set('inst_st', array(
+	#	array('AZ_SunkenTemple_Instance.png', 300, 0),
+	#	array('AZ_SunkenTemple_Instance_lower.png', 0, 400),
+	#));
+
+	assemble_set('raid_aq40', array(
+		array('Ahn_Qiraj.png', 0, 0),
+		array('Ahn_Qiraj__cthun.png', 220, 930),
+	));
+
+	function assemble_set($name, $tiles, $bg_color='black'){
+
+		global $flats, $built;
+
+		echo "$name : ";
+
+		# get canvas size
+		$max_w = 0;
+		$max_h = 0;
+		foreach ($tiles as $tile){
+			list($w, $h) = getimagesize("$flats/$tile[0]");
+			$max_w = max($max_w, $tile[1]+$w);
+			$max_h = max($max_h, $tile[2]+$h);
+		}
+		$w = ceil($max_w / 256) * 256;
+		$h = ceil($max_h / 256) * 256;
+
+		# create canvas
+		$temp = "$flats/temp.png";
+		$cmd = "convert xc:{$bg_color} -geometry !{$w}x{$h} $temp";
+		echo shell_exec($cmd);
+		foreach ($tiles as $tile){
+			$cmd = "composite $flats/$tile[0] -geometry +{$tile[1]}+{$tile[2]} $temp $temp";
+			echo shell_exec($cmd);
+		}
+
+return;
+		# cut out z0 pieces
+
+		$w /= 256;
+		$h /= 256;
+
+		shell_exec("rm -rf $built/$name");
+		mkdir("$built/$name");
+
+		for ($x=0; $x<$w; $x++){
+		for ($y=0; $y<$h; $y++){
+
+			$xp = $x * 256;
+			$yp = $y * 256;
+
+			$out = "$built/$name/tile_z0_".sprintf('%02d_%02d', $x, $y).'.png';
+			$cmd = "convert $temp -crop 256x256+$xp+$yp +repage $out";
+
+			echo shell_exec($cmd);
+			echo '.';
+		}
+		}
+
+		echo "done!\n";
+	}
---
+++ wmo/build.php
@@ -0,0 +1,412 @@
+<?
+	include('../build/config.php');
+
+
+	#build_wmo_map('Dungeon/KL_OrgrimmarLavaDungeon', 'LavaDungeon');
+
+	#build_wmo_map('Dungeon/AZ_Deadmines', 'AZ_Deadmines_A');
+	#build_wmo_map('Dungeon/AZ_Deadmines', 'AZ_Deadmines_B');
+	#build_wmo_map('Dungeon/AZ_Deadmines', 'AZ_Deadmines_C');
+	#build_wmo_map('Dungeon/AZ_Deadmines', 'AZ_Deadmines_D');
+
+	#build_wmo_map('Dungeon/AZ_StormwindPrisons', 'StormwindPrison');
+	#build_wmo_map('Dungeon/AZ_StormwindPrisons', 'StormwindJail');
+	#build_wmo_map('Dungeon/AZ_Subway', 'Subway');
+
+	#build_wmo_map('Dungeon/KL_Wailing', 'wailingcaverns');
+	#build_wmo_map('Dungeon/KL_Wailing', 'wailingcaverns_instance');
+
+	#build_wmo_map('Dungeon/KL_Blackfathom', 'Blackfathom');
+	#build_wmo_map('Dungeon/KL_Blackfathom', 'Blackfathom_instance');
+
+	#build_wmo_map('Dungeon/KZ_Gnomeragon', 'KZ_Gnomeragon_Instance');
+
+	#build_wmo_map('Dungeon/LD_ScarletMonestary' ,'Monestary_Cathedral');
+	#build_wmo_map('Dungeon/LD_ScarletMonestary' ,'Monestary_Cemetery');
+	#build_wmo_map('Dungeon/LD_ScarletMonestary' ,'Monestary_Library');
+	#build_wmo_map('Dungeon/LD_ScarletMonestary' ,'Monestary_War');
+
+	#build_wmo_map('Dungeon/KL_Razorfen', 'RazorfenDowns_instance');
+	#build_wmo_map('Dungeon/KL_Razorfen', 'RazorfenKraul_instance');
+
+	#build_wmo_map('Dungeon/KL_Maraudon', 'KL_Maraudon');
+	#build_wmo_map('Dungeon/KL_Maraudon', 'KL_Maraudon_instance01');
+
+	#build_wmo_map('Dungeon/KZ_Uldaman', 'KZ_Uldaman');
+	#build_wmo_map('Dungeon/KZ_Uldaman', 'KZ_Uldaman_A');
+	#build_wmo_map('Dungeon/KZ_Uldaman', 'KZ_Uldaman_B');
+	#build_wmo_map('Dungeon/KZ_Uldaman', 'KZ_Uldaman_C');
+	#build_wmo_map('Dungeon/KZ_Uldaman', 'KZ_Uldaman_Unearthed');
+
+	#build_wmo_map('Dungeon/KL_DireMaul', 'KL_Diremaul');
+	#build_wmo_map('Dungeon/KL_DireMaul', 'KL_Diremaul_Instance');
+	#build_wmo_map('Dungeon/KL_DireMaul', 'KL_Diremaul__BackEnt');
+
+	#build_wmo_map('Dungeon/LD_Stratholme', 'FrostWyrm_Final01');
+	#build_wmo_map('Dungeon/LD_Stratholme', 'Stratholme');
+	#build_wmo_map('Dungeon/LD_Stratholme', 'Stratholme_A');
+	#build_wmo_map('Dungeon/LD_Stratholme', 'Stratholme_B');
+	#build_wmo_map('Dungeon/LD_Stratholme', 'Stratholme_C');
+	#build_wmo_map('Dungeon/LD_Stratholme', 'Stratholme_raid');
+
+	#build_wmo_map('Dungeon/AZ_Blackrock', 'Blackrock');
+	#build_wmo_map('Dungeon/AZ_Blackrock', 'Blackrock_lower_guild');
+	#build_wmo_map('Dungeon/AZ_Blackrock', 'Blackrock_lower_instance');
+	#build_wmo_map('Dungeon/AZ_Blackrock', 'Blackrock_upper_guild');
+	#build_wmo_map('Dungeon/AZ_Blackrock', 'Blackrock_upper_instance');
+
+	$st_lower = array(
+		101,	# bottom middle
+		3,4,5,6,7,8,9,10,11,12,13,14,27, # outer ring
+		28,29,30,	# bottom side rooms
+		37,38,39,40,41,42,43,44,45,46,47,71, # connecting ring to middle
+		116,117,118,119, # stairs up
+		66,73,78,83,	# ramps
+		104,105, # vertical coridoor
+		120,121	# entrance
+	);
+
+	#build_wmo_map('Dungeon/SunkenTemple', 'AZ_SunkenTemple');
+	#build_wmo_map('Dungeon/SunkenTemple', 'AZ_SunkenTemple_Instance', array(), $st_lower);
+	#build_wmo_map('Dungeon/SunkenTemple', 'AZ_SunkenTemple_Instance', $st_lower, array(), 'AZ_SunkenTemple_Instance_lower');
+	#exit;
+
+	#build_wmo_map('Dungeon/LD_ShadowFang', 'LD_ShadowFang');
+	#build_wmo_map('Dungeon/LD_ShadowFang', 'LD_ShadowFangInterior');
+
+
+	$cthun = array(1,2,23,24,25,26,30,31,32,34);
+	$junk_over = array(
+		22,42, # misc facade
+		33, # brain?
+		44, # random coridoor
+	); # 21&37 are needed :(
+
+	#build_wmo_map('Dungeon/KL_AhnQiraj', 'Ahn_Qiraj_facade');
+	build_wmo_map('Dungeon/KL_AhnQiraj', 'Ahn_Qiraj', array(), array_merge($cthun, $junk_over));
+	#build_wmo_map('Dungeon/KL_AhnQiraj', 'Ahn_Qiraj', $cthun, array(), 'Ahn_Qiraj__cthun');
+
+	function build_wmo_map($folder, $map_name, $only_chunks=array(), $exclude_chunks=array(), $alt_name=''){
+
+		global $blps, $pngs, $flats;
+
+		$out_name = $alt_name ? $alt_name : $map_name;
+
+		echo "$out_name: ";
+
+		$wmo = "$blps/World/wmo/$folder/{$map_name}.wmo";
+
+		#echo "WMO: $wmo\n";
+		#exit;
+
+		$chunks = extract_mogi($wmo);
+
+#foreach ($chunks as $k => $v){
+#	echo "$k,$v[0],$v[1],$v[2]\n";
+#}
+#exit;
+
+		#
+		# filter chunks?
+		#
+
+		if (count($only_chunks)){
+			foreach ($chunks as $k => $v){
+				if (!in_array($k, $only_chunks)) unset($chunks[$k]);
+			}
+		}else if (count($exclude_chunks)){
+			foreach ($chunks as $k => $v){
+				if (in_array($k, $exclude_chunks)) unset($chunks[$k]);
+			}
+		}
+#print_r($chunks);
+
+		#
+		# find out size of all group chunks
+		#
+
+		$png_folder = str_replace('/', '_', $folder);
+		$png_prefix = $map_name.'_';
+
+		$files = glob("$pngs/WMO_$png_folder/$png_prefix*");
+		$pieces = array();
+		$rx = preg_quote($png_prefix, '!').'(\d\d\d)_(\d\d)_(\d\d)\.png$';
+		foreach ($files as $file){
+			if (preg_match("!$rx!", $file, $m)){
+				list($w, $h) = getimagesize($file);
+				$pieces[intval($m[1])][] = array(
+					'path'	=> $file,
+					'group'	=> intval($m[1]),
+					'x'	=> intval($m[2]),
+					'y'	=> intval($m[3]),
+					'w'	=> $w,
+					'h'	=> $h,
+				);
+			}
+		}
+
+
+		#
+		# build a list of group chunks with the total size and the positions of all the subchunks
+		#
+
+		$groups = array();
+		foreach ($pieces as $idx => &$group){
+
+			$total_h = 0;
+			$total_w = 0;
+			$chunk_w = 0;
+			$chunk_h = 0;
+
+			foreach ($group as $row){
+				if ($row['x'] == 0) $total_h += $row['h'];
+				if ($row['y'] == 0) $total_w += $row['w'];
+				$chunk_w = max($chunk_w, $row['x']+1);
+				$chunk_h = max($chunk_h, $row['y']+1);
+			}
+
+			$our_pngs = array();
+
+			$y_pos = $total_h;
+			for ($y=0; $y<$chunk_h; $y++){
+				$x_pos = 0;
+				for ($x=0; $x<$chunk_w; $x++){
+					foreach ($group as $row){
+						if ($row['x'] == $x && $row['y'] == $y){
+							$our_pngs[] = array($row['path'], $row['w'], $row['h'], $x_pos, $y_pos-$row['h']);
+						}
+					}
+
+					$x_pos += 256;
+				}
+				$y_pos -= 256;
+			}
+
+			$groups[$idx] = array(
+				'w'	=> $total_w,
+				'h'	=> $total_h,
+				'pngs'	=> $our_pngs,
+			);
+		}
+
+		#print_r($groups);
+		#exit;
+
+
+		#
+		# need to double chunk coords
+		#
+
+		foreach ($chunks as $k => $pos){
+			$chunks[$k][0] *= 1.993;
+			$chunks[$k][1] *= 1.993;
+		}
+
+
+		#
+		# adjust chunk positions so they start in top left
+		#
+
+		$x1s = array();
+		$y1s = array();
+		$x2s = array();
+		$y2s = array();
+
+		foreach ($chunks as $k => $pos){
+			$chunks[$k][1] -= $groups[$k]['h'];
+
+			$x1s[] = $chunks[$k][0];
+			$y1s[] = $chunks[$k][1];
+
+			$x2s[] = $chunks[$k][0] + $groups[$k]['w'];
+			$y2s[] = $chunks[$k][1] + $groups[$k]['h'];
+		}
+
+		$min_x = min($x1s);
+		$min_y = min($y1s);
+
+		foreach ($chunks as $k => $pos){
+			$chunks[$k][0] -= $min_x;
+			$chunks[$k][1] -= $min_y;
+		}
+
+		$canvas_w = ceil(max($x2s) - $min_x);
+		$canvas_h = ceil(max($y2s) - $min_y);
+
+#print_r($chunks);
+#print_r($groups);
+		$dst = "$flats/{$out_name}.png";
+
+		echo shell_exec("convert -size !{$canvas_w}x{$canvas_h} null: -matte -compose Clear -composite -compose Over $dst");
+		#echo shell_exec("convert xc:{$bg_color} -geometry !{$canvas_w}x{$canvas_h} $dst");
+
+		uasort($chunks, 'zsort_chunks');
+
+		foreach ($chunks as $k => $chunk){
+
+			if (is_array($groups[$k]['pngs']))
+			foreach ($groups[$k]['pngs'] as $row){
+
+				$x = floor($chunk[0] + $row[3]);
+				$y = floor($chunk[1] + $row[4]);
+
+				$cmd = "composite $row[0] -geometry +{$x}+{$y} $dst $dst";
+				#echo "$cmd\n";
+				echo shell_exec($cmd);
+				echo '.';
+			}
+		}
+		echo shell_exec("convert $dst -rotate 90 $dst");
+		echo " done\n";
+	}
+
+	function zsort_chunks($a, $b){
+		return $a[2]-$b[2];
+	}
+
+
+
+	function extract_pos($name){
+
+		$fh = fopen($name, 'r');
+		fseek($fh, 24);
+		$floats = fread($fh, 24);
+		$box = unpack('f6', $floats);
+
+		$x1 = $box[1];
+		$y1 = $box[3];
+
+		$x2 = $box[4];
+		$y2 = $box[6];
+
+		return "$x1,$y1 -> $x2,$y2";
+	}
+
+	function extract_group_id($name){
+
+		$fh = fopen($name, 'r');
+		fseek($fh, 20 + 0x38);
+		$id = read_int($fh);
+		fclose($fh);
+
+		return $id;
+	}
+
+	function extract_root_id($name){
+
+		$fh = fopen($name, 'r');
+		fseek($fh, 20 + 0x20);
+		$id = read_int($fh);
+		fclose($fh);
+
+		return $id;
+	}
+
+
+	function chunk_map($fh, $seek=1){
+
+		if ($seek){
+			$offset = 0;
+			fseek($fh, 0);
+		}else{
+			$offset = ftell($fh);
+		}
+
+		while (!feof($fh)){
+
+			$type = fread($fh, 4);
+			if (strlen($type) < 4) return;
+			$type = strrev($type);
+
+			$size = read_int($fh);
+			fseek($fh, $size, SEEK_CUR);
+
+			echo "$type: $size (@$offset)\n";
+
+			$offset += 8 + $size;
+		}
+	}
+
+	function seek_to_chunk($fh, $name){
+
+		fseek($fh, 0);
+		while (!feof($fh)){
+
+			$type = fread($fh, 4);
+			if (strlen($type) < 4) break;
+			$type = strrev($type);
+
+			$size = read_int($fh);
+			if ($type == $name) return true;
+
+			fseek($fh, $size, SEEK_CUR);
+		}
+
+		return false;
+	}
+
+
+
+	function read_int($fh){
+                $data = fread($fh, 4);
+                list($junk, $n) = unpack('V', $data);
+                return $n;
+        }
+
+
+	function extract_mogi($filename){
+
+		$fh = fopen($filename, 'r');
+
+		seek_to_chunk($fh, 'MOHD');
+		fseek($fh, 4, SEEK_CUR);
+		$groups = read_int($fh);
+
+		seek_to_chunk($fh, 'MOHD');
+		fseek($fh, 0x24, SEEK_CUR);
+		$floats = fread($fh, 24);
+		$box = unpack('f6', $floats);
+		#echo "root\t\t: ".format_box($box)."\n";
+
+		$chunks = array();
+
+		seek_to_chunk($fh, 'MOGI');
+		for ($i=0; $i<$groups; $i++){
+
+			$flags = read_int($fh);
+			$floats = fread($fh, 24);
+			$box = unpack('f6', $floats);
+			$name = read_int($fh);
+
+			$chunks[$i] = array($box[1], 0-$box[2], $box[3]);
+
+			#echo "group $i \t: ".format_box($box)."\n";
+			#print_r($box);
+		}
+
+		#echo "found $groups groups\n";
+		#exit;
+
+		fclose($fh);
+
+		return $chunks;
+	}
+
+
+function format_box($box){
+	$x1 = str_pad(round($box[1]), 4, ' ', STR_PAD_LEFT);
+	$y1 = str_pad(round($box[2]), 4, ' ', STR_PAD_LEFT);
+	$z1 = str_pad(round($box[3]), 4, ' ', STR_PAD_LEFT);
+	$x2 = str_pad(round($box[4]), 4, ' ', STR_PAD_LEFT);
+	$y2 = str_pad(round($box[5]), 4, ' ', STR_PAD_LEFT);
+	$z2 = str_pad(round($box[6]), 4, ' ', STR_PAD_LEFT);
+
+
+	return round($box[1]).','.round(0-$box[2]);
+
+	$from = round(27+32+0-$box[6]).','.round(103+$box[1]);
+	$to = round(27+32+0-$box[3]).','.round(103+$box[4]);
+	return "$from - $to";
+
+	return "($x1, $y1, $z1) - ($x2, $y2, $z2)";
+}
