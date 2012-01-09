a1719fac6907c3b6331e068e5af3ad05bcd4d84d
Cal Henderson <cal@iamcal.com>
2012-01-09 13:23:43 -0800

    rename all extracted files to lowercase so everything is grouped correctly

---
+++ build/0_rename.php
@@ -0,0 +1,49 @@
+<?
+	# run this after you're extracted the files from the MPQs.
+	# if changes all paths to lowercase so they're grouped correctly.
+
+	include('config.php');
+
+
+	#
+	# get file list
+	#
+
+	echo "searching... ";
+
+	chdir($blps_raw);
+	$files = explode("\n", trim(shell_exec("find .")));
+
+	$num = count($files);
+	echo "found $num files\n";
+
+
+	#
+	# move files
+	#
+
+	echo "moving... ";
+
+	#$time = time();
+	#shell_exec("mv $blps {$blps}_backup_$time");
+	#mkdir($blps);
+
+	foreach ($files as $file){
+		if (!preg_match('!\.(blp|wmo)$!i', $file)) continue;
+
+		$file = substr($file, 1);
+		$low = "$blps".StrToLower($file);
+		$src = $blps_raw.$file;
+
+		$bits = explode('/', $low);
+		array_pop($bits);
+		$path = implode('/', $bits);
+
+		@mkdir($path, 0777, true);
+		rename($src, $low);
+	}
+
+	echo "done\n";
+
+
+#print_r($ret);
