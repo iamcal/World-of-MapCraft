564bfa83ddbb5ad079bb07cb4f427047e1000df6
Cal Henderson <cal@iamcal.com>
2012-01-19 20:21:20 -0800

    crushing stuff

---
+++ build/7_crush.php
@@ -0,0 +1,62 @@
+<?
+	include('config.php');
+
+	chdir($built);
+	$lines = explode("\n", shell_exec("find ."));
+
+	echo "found ".count($lines)." lines\n";
+
+	$for_real = array();
+
+	foreach ($lines as $line){
+		if (!strlen($line)) continue;
+		if (!preg_match('!\.png$!', $line)) continue;
+		if (preg_match('!--16\.png$!', $line)) continue;
+
+		$src = $built.'/'.$line;
+		$dst = $crushed.'/'.$line;
+
+		if (file_exists($dst)) continue;
+
+		$for_real[] = array($src, $dst, $line);
+	}
+
+	echo "processing ".count($for_real)." images : ";
+
+	foreach ($for_real as $row){
+
+		list($src, $dst, $name) = $row;
+		$tmp = '/tmp/crush.png';
+
+		@mkdir(dirname($dst), 0777, true);
+		$cmd = "/root/pngout-20110722-linux-static/i686/pngout-static $src $tmp -y";
+
+		$out = array();
+		exec($cmd, $out, $code);
+
+		if ($code != 0){
+			$out_flat = implode("\n", $out);
+
+			if ($code == 1 && preg_match('!unsupported format!', $out_flat)){
+
+				echo "x($name)";
+				#copy($src, $tmp);
+
+			}else if ($code == 2 && preg_match('!Unable to compress further: copying original file!', $out_flat)){
+
+				echo '0';
+				copy($src, $dst);
+			}else{
+				echo "$cmd\n";
+				print_r($out);
+				echo "code: $code\n";
+				#exit;
+			}
+		}else{
+			rename($tmp, $dst);
+
+			echo ".";
+		}
+	}
+
+	echo " done\n";
---
+++ build/colors.php
@@ -0,0 +1,17 @@
+<?
+	$colors = array(
+		'azeroth'	=> '#001D29',
+		'vashjir'	=> '#575357',
+		'deepholm'	=> '#2E2D36',
+
+		'bg_wsg'	=> '#F7F3F7',
+		'bg_ab'		=> '#414318',
+		'bg_av'		=> '#8FB0C9',
+		'bg_eots'	=> '#000000',
+		'bg_sota'	=> '#052431',
+		'bg_ioc'	=> '#1E395D',
+		'bg_bfg'	=> '#000C18',
+		'bg_tp'		=> '#8E8175',
+		
+	);
+?>
