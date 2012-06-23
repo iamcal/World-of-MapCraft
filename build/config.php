a1849343b0018c81f19ef2baa9cf31516d38dda4
Cal Henderson <cal@iamcal.com>
2012-06-22 22:56:40 -0700

    building the MoP tilesets

--- build/config.php
+++ build/config.php
@@ -3,47 +3,47 @@
 	# path where we'll extract the BLP files
 	#
 
-	$blps_raw	= "/var/www/doats.net/tiles/out";
-	$blps		= "/var/www/doats.net/tiles/out_fixed";
+	$blps_raw	= "/var/www/doats.net/tiles/mop_out";
+	$blps		= "/var/www/doats.net/tiles/mop_out";
 
 
 	#
 	# path where we'll convert BLPs to PNGs, along with URL to it
 	#
 
-	$pngs = "/var/www/doats.net/tiles/pngs_fixed";
-	$pngs_url = "/tiles/pngs_fixed";
+	$pngs = "/var/www/doats.net/tiles/mop_pngs";
+	$pngs_url = "/tiles/mop_pngs";
 
 
 	#
 	# path to the preview maps
 	#
 
-	$maps = "/var/www/doats.net/tiles/maps";
+	$maps = "/var/www/doats.net/tiles/mop_maps";
 
 
 	#
 	# flat versions of composited images
 	#
 
-	$flats = "/var/www/doats.net/tiles/flats";
+	$flats = "/var/www/doats.net/tiles/mop_flats";
 
 
 	#
 	# path to the built tilesets
 	#
 
-	$built = "/var/www/doats.net/tiles/built";
-	$built_url = "/tiles/built";
+	$built = "/var/www/doats.net/tiles/mop_built";
+	$built_url = "/tiles/mop_built";
 
-	$crushed = "/var/www/doats.net/tiles/crushed";
+	$crushed = "/var/www/doats.net/tiles/mop_crushed";
 
 
 	#
 	# other stuff
 	#
 
-	$bg_url = "/tiles/purple-stripes.png";
+	$bg_url = "/tiles/www/purple-stripes.png";
 
 	$blp_convertor = "/root/Kanma-BLPConverter-3abb6b9/build/bin/BLPConverter";
 
