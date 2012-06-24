05966965c6c8edcb0cd734e7cd4a2370aa54a0cb
Cal Henderson <cal@iamcal.com>
2012-06-24 12:48:55 -0700

    updated extract script for MoP

--- extract.sh
+++ extract.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
 /root/git-projects/MPQExtractor/build/bin/MPQExtractor \
-	-e "World\Minimaps\*" -f -p \
-	mpqs/wow-update-13164.MPQ,base \
-	mpqs/wow-update-13205.MPQ,base \
-	mpqs/wow-update-13287.MPQ,base \
-	mpqs/wow-update-13329.MPQ,base \
-	mpqs/wow-update-13596.MPQ,base \
-	mpqs/wow-update-13623.MPQ,base \
-	mpqs/wow-update-base-13914.MPQ \
-	mpqs/wow-update-base-14007.MPQ \
-	mpqs/wow-update-base-14333.MPQ \
-	mpqs/wow-update-base-14480.MPQ \
-	mpqs/wow-update-base-14545.MPQ \
-	mpqs/wow-update-base-14946.MPQ \
-	mpqs/wow-update-base-15005.MPQ \
-	mpqs/wow-update-base-15050.MPQ \
-	-o out mpqs/art.MPQ
+	-e "World\Minimaps\*" -f -c -p \
+	mop_mpqs/expansion1.MPQ \
+	mop_mpqs/expansion2.MPQ \
+	mop_mpqs/expansion3.MPQ \
+	mop_mpqs/expansion4.MPQ \
+	mop_mpqs/wow-update-base-15508.MPQ \
+	mop_mpqs/wow-update-base-15544.MPQ \
+	mop_mpqs/wow-update-base-15589.MPQ \
+	mop_mpqs/wow-update-base-15640.MPQ \
+	mop_mpqs/wow-update-base-15650.MPQ \
+	mop_mpqs/wow-update-base-15657.MPQ \
+	mop_mpqs/wow-update-base-15662.MPQ \
+	mop_mpqs/wow-update-base-15668.MPQ \
+	mop_mpqs/wow-update-base-15677.MPQ \
+	mop_mpqs/wow-update-base-15689.MPQ \
+	mop_mpqs/wow-update-base-15699.MPQ \
+	mop_mpqs/wow-update-base-15726.MPQ \
+	mop_mpqs/wow-update-base-15739.MPQ \
+	mop_mpqs/wow-update-base-15752.MPQ \
+	mop_mpqs/wow-update-base-15762.MPQ \
+	mop_mpqs/wow-update-base-15781.MPQ \
+	-o mop_out mop_mpqs/texture.MPQ
 
 /root/git-projects/MPQExtractor/build/bin/MPQExtractor \
-	-e "World\WMO\*" -f -p \
-	mpqs/expansion1.MPQ \
-	mpqs/expansion2.MPQ \
-	mpqs/expansion3.MPQ \
-	mpqs/wow-update-13164.MPQ,base \
-	mpqs/wow-update-13205.MPQ,base \
-	mpqs/wow-update-13287.MPQ,base \
-	mpqs/wow-update-13329.MPQ,base \
-	mpqs/wow-update-13596.MPQ,base \
-	mpqs/wow-update-13623.MPQ,base \
-	mpqs/wow-update-base-13914.MPQ \
-	mpqs/wow-update-base-14007.MPQ \
-	mpqs/wow-update-base-14333.MPQ \
-	mpqs/wow-update-base-14480.MPQ \
-	mpqs/wow-update-base-14545.MPQ \
-	mpqs/wow-update-base-14946.MPQ \
-	mpqs/wow-update-base-15005.MPQ \
-	mpqs/wow-update-base-15050.MPQ \
-	-o out mpqs/world.MPQ
+	-e "World\WMO\*" -f -c -p \
+	mop_mpqs/expansion1.MPQ \
+	mop_mpqs/expansion2.MPQ \
+	mop_mpqs/expansion3.MPQ \
+	mop_mpqs/expansion4.MPQ \
+	mop_mpqs/wow-update-base-15508.MPQ \
+	mop_mpqs/wow-update-base-15544.MPQ \
+	mop_mpqs/wow-update-base-15589.MPQ \
+	mop_mpqs/wow-update-base-15640.MPQ \
+	mop_mpqs/wow-update-base-15650.MPQ \
+	mop_mpqs/wow-update-base-15657.MPQ \
+	mop_mpqs/wow-update-base-15662.MPQ \
+	mop_mpqs/wow-update-base-15668.MPQ \
+	mop_mpqs/wow-update-base-15677.MPQ \
+	mop_mpqs/wow-update-base-15689.MPQ \
+	mop_mpqs/wow-update-base-15699.MPQ \
+	mop_mpqs/wow-update-base-15726.MPQ \
+	mop_mpqs/wow-update-base-15739.MPQ \
+	mop_mpqs/wow-update-base-15752.MPQ \
+	mop_mpqs/wow-update-base-15762.MPQ \
+	mop_mpqs/wow-update-base-15781.MPQ \
+	-o mop_out mop_mpqs/world.MPQ
